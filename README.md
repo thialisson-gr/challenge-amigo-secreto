32117d8b9452c7ac55736f3beb12057cc3d541e7
Thialisson <177430526+thialisson-gr@users.noreply.github.com>
2025-02-11 11:00:12 -0300

    Fim do projeto

---
+++ README.md
@@ -0,0 +1,46 @@
+# Challenge do Amigo Secreto
+
+Este é um projeto desenvolvido como parte do programa Oracle Next Education. O objetivo do projeto é criar uma aplicação para sortear amigos secretos.
+
+## Funcionalidades
+
+- Cadastro de participantes
+- Sorteio de amigos secretos
+- Envio de notificações aos participantes
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Executar o Projeto
+
+1. Clone o repositório:
+    ```bash
+    git clone https://github.com/seu-usuario/challenge-do-amigo-secreto.git
+    ```
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd challenge-do-amigo-secreto
+    ```
+3. Abra o arquivo `index.html` no seu navegador.
+
+## Contribuição
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir uma issue ou enviar um pull request.
+
+## Licença
+
+Este projeto está licenciado sob a Licença MIT.
+
+## Contato
+
+Para mais informações, entre em contato com [thialissongomes@outlook.com].
+
+## Imagens
+
+Aqui está uma imagem do projeto:
+
+![Tela Inicial](./images/home%20page.png)
+
--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  //captura o valor do campo de entrada
+  let nome = document.getElementById("amigo").value.trim();
+
+  //Valida se o nome não está vazio
+  if (nome !== "") {
+    amigos.push(nome);
+    console.log(amigos);
+
+    atualizarLista();
+  } else {
+    alert("Por favor, insira um nome válido!");
+  }
+
+  document.getElementById("amigo").value = "";
+}
+
+//Atualiza a lista de pessoas e limpa para inserção de um novo nome.
+function atualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+
+    item.textContent = amigos[i];
+
+    lista.appendChild(item);
+  }
+}
+
+//Verifica se a lista está vazia, sortea um nome, e mostra no elemento da página.
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Sua lista de amigos está vazia!");
+  } else {
+    let sorteio = Math.floor(Math.random() * amigos.length);
+
+    console.log(sorteio);
+    let sorteado = amigos[sorteio];
+
+    console.log(amigos[sorteio]);
+
+    document.getElementById("resultado").innerHTML = sorteado;
+  }
+}
